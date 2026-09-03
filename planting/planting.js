353705dc041d4c6660c2a0998209ff6c244dd765
sebamirandamiranda-ship-it <sebamiranda.miranda@gmail.com>
2026-09-03 16:29:32 -0300

    Update planting.js

--- planting/planting.js
+++ planting/planting.js
@@ -34,8 +34,8 @@ const btnGuardarSiembra =
   document.getElementById(
     "btnGuardarSiembra"
   );
-btnCancelarSiembra.onclick =
-  cerrarModalSiembra;
+
+
 const filtroCrop =
   document.getElementById("filtroCrop");
 
