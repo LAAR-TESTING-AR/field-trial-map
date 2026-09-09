ece4beb37293a8160cedefaff9f9154b2cbf2ca5
sebamirandamiranda-ship-it <sebamiranda.miranda@gmail.com>
2026-09-09 10:50:18 -0300

    Update planting.js

--- planting/planting.js
+++ planting/planting.js
@@ -701,33 +701,23 @@ function crearPopup(sitio, estado) {
     limpiarTexto(
       sitio["Planting Date (MM/DD/YYYY)"]
     );
-  
-const esViewer =
-  window.FieldTrialAppMode &&
-  window.FieldTrialAppMode.isViewer;
-  
+
   let bloqueAccion = "";
 
-if (estado === "PENDIENTE") {
-
-  if (!esViewer) {
+  if (estado === "PENDIENTE") {
 
     bloqueAccion = `
 
       <hr>
 
       <button
-        type="button"
-        class="btn-registrar-siembra"
-        data-aoi="${aoiId}">
-        Registrar Siembra
-      </button>
+  type="button"
+  class="btn-registrar-siembra"
+  data-aoi="${aoiId}">
+  Registrar Siembra
+</button>
 
     `;
-
-  }
-
-}
 
   } else if (estado === "SEMBRADO") {
 
